e8cf4752e2daeb3a0b9ab537f74470289993fc79
devops <devops@heraizen.com>
2022-07-01 19:54:00 +0530

    Vector skin added

---
+++ skins/Vector/vendor/composer/installed.php
@@ -0,0 +1,35 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+      0 => '1.x-dev',
+    ),
+    'reference' => 'd1ae39c62b950f12d5e05618120afd4dd9d2fc4b',
+    'name' => 'mediawiki/vector-skin',
+  ),
+  'versions' => 
+  array (
+    'composer/installers' => 
+    array (
+      'pretty_version' => 'v2.1.1',
+      'version' => '2.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'af93ba6e52236418f07a278033eba6959ee5b983',
+    ),
+    'mediawiki/vector-skin' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '1.x-dev',
+      ),
+      'reference' => 'd1ae39c62b950f12d5e05618120afd4dd9d2fc4b',
+    ),
+  ),
+);
